22b165dd09b11adb57bf71e3636b091f3bba8202
Leonardo Korndorfer <leokorndorfer@gmail.com>
2015-09-13 22:14:37 -0300

    reorganizing and adding

---
+++ Assets/DistanceCalculator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceCalculator : MonoBehaviour {
+	public GameObject obj;
+
+	private Vector3 lastPosition;
+	private float   traveledDistance;
+
+	private float   totalTime;
+	private float   nextInterval;
+
+	// Use this for initialization
+	void Start () {
+		this.lastPosition = obj.transform.position;
+		this.traveledDistance = 0;
+
+		this.totalTime = Time.time;
+		this.nextInterval = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.traveledDistance += Vector3.Distance(this.lastPosition, obj.transform.position);
+		this.lastPosition = obj.transform.position;
+
+		// Uncomment this to know how many meters is running pr second
+		this.totalTime += Time.deltaTime;
+		if (this.totalTime > this.nextInterval) {
+			this.nextInterval += 1;
+			Debug.Log (this.traveledDistance + " " + this.totalTime);
+		}
+	}
+}
---
+++ Assets/Scripts/Scenario/DestroyPlatform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyPlatform : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// Debug.Log ("Destroy platform");
+
+		if (other.gameObject.transform.parent) {
+			Destroy (other.gameObject.transform.parent.gameObject);
+		} else {
+			Destroy (other.gameObject);
+		}
+	}
+}
