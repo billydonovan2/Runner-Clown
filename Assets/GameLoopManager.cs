f815120e4127b1c2351206b606e716737781d14a
Leonardo Korndorfer <leokorndorfer@gmail.com>
2015-09-25 20:13:51 -0300

    Adding start menu

---
+++ Assets/GameLoopManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoopManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void restartLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ Assets/Music/README.txt
@@ -0,0 +1,2 @@
+http://opengameart.org/content/heavy-metal-3hrmt3
+http://opengameart.org/content/abelian
